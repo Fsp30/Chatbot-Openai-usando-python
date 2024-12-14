7b1b9cdf788ffc9b317ef372fc8250c53d1462cb
Filipe de Paula <164094327+Fsp30@users.noreply.github.com>
2024-12-13 22:37:32 -0300

    Update chatbot.py
    retirando comentarios que fiz ao longo do desenvolvimento

--- chatbot.py
+++ chatbot.py
@@ -2,30 +2,24 @@ import openai
 import os
 from dotenv import load_dotenv
 
-# Carregar variáveis de ambiente
 load_dotenv()
 api_key = os.getenv('API_KEY')
 openai.api_key = api_key
 
-# Função para enviar mensagens
 def input_message(message, list_messages):
     list_messages.append({
         "role": "user",
         "content": message
     })
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # Modelo corrigido
+        model="gpt-3.5-turbo", 
         messages=list_messages
     )
-    # Adicionar resposta do chatbot à lista de mensagens
-    bot_message = response.choices[0].message  # Atualização para a nova API
+    bot_message = response.choices[0].message
     list_messages.append(bot_message)
     return bot_message
-
-# Lista para armazenar mensagens da conversa
 list_messages = []
 
-# Loop principal
 while True:
     message = input("Your Questions: ")
     if message.lower() == 'exit':
